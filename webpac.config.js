f21c81e03ae097c3916818b173fcb9754584e6b4
mini2809 <minijan928@gmail.com>
2020-06-18 23:08:34 -0400

    capstone project started

---
+++ webpac.config.js
@@ -0,0 +1,11 @@
+const path = require('path')  
+const webpack = require('webpack')
+
+module.exports={
+	entry:['./src/client/index.js'],
+	mode:'development',
+	module:{
+		rules:[]
+	},
+	plugins:[]
+}
